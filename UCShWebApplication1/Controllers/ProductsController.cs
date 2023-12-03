64f2f1007351111e7320d34efad884a4b801e26a
Ccharliiee <27688952+Ccharliiee@users.noreply.github.com>
2023-12-02 22:19:09 -0500

    Create ProductsController.cs
    add 2 api endpoints

---
+++ UCShWebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UCShWebApplication1.DbConnectHelper;
+using UCShWebApplication1.Entities;
+
+namespace UCShWebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProStoreContext _vproStoreContext;
+        public ProductsController(ProStoreContext vproStoreContext)
+        {
+            _vproStoreContext = vproStoreContext;
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            return  _vproStoreContext.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product?> GetProduct(int id)
+        {
+            return _vproStoreContext.Products.Find(id);
+        }
+    }
+}
+
